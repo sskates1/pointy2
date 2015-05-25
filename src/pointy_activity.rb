21de4214fdc45cd3f3e0bbcedd91c35379aab777
sskates1 <simonirv2@gmail.com>
2015-05-25 04:06:39 -0400

    Temp commit. Crashes on launch

--- src/pointy_activity.rb
+++ src/pointy_activity.rb
@@ -9,7 +9,7 @@ ruboto_import_widgets :Button, :LinearLayout, :TextView
 class PointyActivity
   def onCreate(bundle)
     super
-    set_title 'Domo arigato, Mr Ruboto!'
+    set_title 'Pointy!'
 
     self.content_view =
         linear_layout :orientation => :vertical do
@@ -18,7 +18,7 @@ class PointyActivity
                                  :gravity => :center, :text_size => 48.0
           button :text => 'M-x butterfly',
                  :layout => {:width => :match_parent},
-                 :id => 43, :on_click_listener => proc { butterfly }
+                 :id => 43, :on_click_listener => proc { rain_arizona }
         end
   rescue Exception
     puts "Exception creating activity: #{$!}"
@@ -27,9 +27,30 @@ class PointyActivity
 
   private
 
-  def butterfly
-    @text_view.text = 'What hath Matz wrought!'
-    toast 'Flipped a bit via butterfly'
+  def get_location(cactus_type=nil)
+     # get longitude latitude of cactus
+     # get location of cactus type
+     if cactus_type == nil
+         long = '31.8500'
+         lat = '-111.0000'
+         return long, lat
+     end
+  end
+
+  def rain_arizona
+      ForecastIO.configure do |configuration|
+          configuration.api_key = ENV['FORECAST_IO_API_KEY']
+      end
+      long, lat = get_location
+      # get forcast of  long, lat
+      forecast = ForecastIO.forecast(long, lat)
+      if forecast.currently.precipProbability > 0
+          @text_view.text = """Its raining in Arizona.
+                              You should water your cactus."""
+      else
+          @text_view.text = """It's not raining in Arizona.
+                              Don't water your cactus today!"""
+      end
   end
 
 end
