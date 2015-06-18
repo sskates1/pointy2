254664d7dd1e7355d91c0c161e119f6f90f527ed
sskates1 <simonirv2@gmail.com>
2015-06-18 11:14:57 -0400

    changed away from forecast gem moved to uri call. Bundler broken

--- src/pointy_activity.rb
+++ src/pointy_activity.rb
@@ -1,6 +1,8 @@
 require 'ruboto/widget'
 require 'ruboto/util/toast'
-require 'forecast_io'
+# require 'open-uri'
+require 'json'
+# require 'forecast_io'
 
 ruboto_import_widgets :Button, :LinearLayout, :TextView
 
@@ -13,10 +15,10 @@ class PointyActivity
 
     self.content_view =
         linear_layout :orientation => :vertical do
-          @text_view = text_view :text => 'What hath Matz wrought?', :id => 42,
+          @text_view = text_view :text => '', :id => 42,
                                  :layout => {:width => :match_parent},
                                  :gravity => :center, :text_size => 48.0
-          button :text => 'M-x butterfly',
+          button :text => 'Should I water my Cactus?',
                  :layout => {:width => :match_parent},
                  :id => 43, :on_click_listener => proc { rain_arizona }
         end
@@ -31,22 +33,28 @@ class PointyActivity
      # get longitude latitude of cactus
      # get location of cactus type
      if cactus_type == nil
-         long = '31.8500'
-         lat = '-111.0000'
-         return long, lat
+         long = '-111.0000'
+         lat = '31.8500'
+         return lat, long
      end
   end
 
   def rain_arizona
-      ForecastIO.configure do |configuration|
-          configuration.api_key = ENV['FORECAST_IO_API_KEY']
-      end
-      long, lat = get_location
-      # get forcast of  long, lat
-      forecast = ForecastIO.forecast(long, lat)
-      if forecast.currently.precipProbability > 0
+    #   ForecastIO.configure do |configuration|
+    #       configuration.api_key = ENV['FORECAST_IO_API_KEY']
+    #   end
+    api_key =
+      lat, long = get_location
+    #   # get forcast of  long, lat
+    #   forecast = ForecastIO.forecast(long, lat)
+    # if forecast.currently.precipProbability > 0
+    response = open('https://api.forecast.io/forecast/APIKEY/lat,long')
+    responseHash = JSON.parse(respeonse.read)
+    precipProbability = responseHash['currently']['precipProbability']
+
+      if  precipProbability > 0
           @text_view.text = """Its raining in Arizona.
-                              You should water your cactus."""
+                              You shouldr water your Cactus."""
       else
           @text_view.text = """It's not raining in Arizona.
                               Don't water your cactus today!"""
